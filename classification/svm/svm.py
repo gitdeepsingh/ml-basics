1c11f861b0d1581221b6251a192f66a22350f966
deepsingh <deepsingh.cts@gmail.com>
2025-01-12 18:13:31 +0530

    svm

---
+++ classification/svm/svm.py
@@ -0,0 +1,97 @@
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+print('reading the dataset.')
+dataset = pd.read_csv('Social_Network_Ads.csv')
+
+print('setting X inputs and y target.')
+X = dataset.iloc[:, :-1].values # all but last col i.e purchase
+y = dataset.iloc[:, -1].values # purchase
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+print('splitting the dataset into the training and test sets.')
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0) #random_state: this fixes the seed, at seed 0, randomly picked test datas are always same.
+
+
+# feature scaling (Standardization)
+from sklearn.preprocessing import StandardScaler
+print('standardizing the training set for feature scaling.')
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+# print(f'X_train= {X_train}')
+# print(f'X_test= {X_test}')
+
+from sklearn.svm import SVC
+classifier = SVC(kernel = 'linear', random_state=0)
+'''
+# SVM uses a technique called the kernel trick. Here, the kernel takes a low-dimensional input space and 
+transforms it into a higher dimensional space. In other words, 
+you can say that it converts nonseparable problem to separable problems by adding more dimension to it
+# linear kernel defines the dot product between the input vectors in the original feature space.
+
+'''
+classifier.fit(X_train, y_train)
+
+# Predicting a new result
+print('Predicting a new result')
+print(classifier.predict(sc.transform([[30,87000]])))
+
+# Predicting the Test set results
+print('predicting the test dataset results...')
+try:     
+    y_pred = classifier.predict(X_test)
+    print('writing the results as [[predictedValue groundTruthValue]] into predicted_outputs.txt ...')
+    pred_res = np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1)
+    with open("predicted_outputs.txt", "w") as file:
+        file.write(str(pred_res))
+except Exception as e:
+    print(f'Error while predicting the test results. Reason= {e}') # gives error if we dont do sc.transform(X_test)
+
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(y_test, y_pred)
+acc_score = accuracy_score(y_test, y_pred)
+print(f'accuracy_score = {acc_score}\nconfusion matrix=\n{cm}')
+
+#%%
+# Visualising the Training set results
+print('visualising the training set results....')
+from matplotlib.colors import ListedColormap
+X_set, y_set = sc.inverse_transform(X_train), y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
+                     np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 0.25))
+plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('SVM (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+print('visualising the test set results....')
+X_set, y_set = sc.inverse_transform(X_test), y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
+                     np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 0.25))
+plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('SVM (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+# %%
